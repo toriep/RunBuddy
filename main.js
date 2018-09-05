cb1be4c3995c2dde6fe3946f6133bb793dbe6c1c
Sarah Han <sarah.s.han@gmail.com>
2018-09-05 00:38:58 -0700

    jsDoc finished

---
+++ main.js
@@ -0,0 +1,120 @@
+
+/**
+ * Listen for the document to load and initialize the application
+ */
+$(document).ready(initializeApp);
+
+/**
+ * global variables here if any 
+ */
+
+ /***************************************************************************************************
+* initializeApp 
+* @params {undefined} none
+* @returns: {undefined} none
+* initializes the application
+*/
+function initializeApp(){
+    
+}
+
+/***************************************************************************************************
+* addClickHandlerstoElements
+* @params {undefined} 
+* @returns  {undefined}
+*     
+*/
+function addClickHandlersToElements(){
+    
+}
+
+/***************************************************************************************************
+ * displayMapToDom - display map based on the the location (based on zip code or city user inputs)
+ * @param: location
+ * @returns: none
+ * @calls: none
+ */
+function displayMapOnDom ( location ) {
+
+}
+
+/***************************************************************************************************
+ * renderyDirection - display direction based on the the pick
+ * @param: pick
+ * @returns: none
+ * @calls: none
+ */
+function renderDirectionOnDom ( pick ) {
+
+}
+
+/***************************************************************************************************
+ * renderAvailableLocationsForRunningToDom - display available locations for running based on yelp database
+ * @param: location
+ * @returns: none
+ * @calls: none
+ */
+function renderAvailableLocationsForRunningOnDom ( location ) {
+
+}
+
+/***************************************************************************************************
+ * renderLocationPicturesToDom - display pictures of the location
+ * @param: location
+ * @returns: none
+ * @calls: none
+ */
+function renderLocationPicturesOnDom ( location ) {
+
+}
+
+/***************************************************************************************************
+ * renderyWeatherToDom - display weather based on the location
+ * @param: location
+ * @returns: none
+ * @calls: none
+ */
+function renderWeatherOnDom ( location ) {
+
+}
+
+/***************************************************************************************************
+ * renderCrimeDataToDom - display crime data based on the location to show how safe 
+ * it is to go running in the location
+ * @param: location
+ * @returns: none
+ * @calls: none
+ */
+function renderCrimeDataOnDom ( location ) {
+
+}
+
+
+/***************************************************************************************************
+ * get data from each server
+ * 
+ * 
+*/
+function getDataFromGoogleMap() {
+
+}
+
+function getDataFromYelp() {
+
+}
+
+function getDataFromCrimeData() {
+
+}
+
+function getDataFromWeather() {
+
+}
+
+function displaySuccess() {
+
+}
+
+function displayError() {
+
+}
