621827b9e7a29444d6ddf6dc27519c5b8d3a44bd
Torie Pham <tientp@uci.edu>
2018-09-05 20:32:45 -0700

    renders meetup info on dom

--- Torie_functions.js
+++ Torie_functions.js
@@ -4,26 +4,26 @@
     <div class="time">7:00AM</div>
     <h4 class="groupName">OC HIKING CLUB: ORANGE COUNTY'S HIKING & BACKPACKING GROUP</h4>
     <a href="https://www.meetup.com/Sunrise-to-Sunset-Hikers/events/252851015/">Step Up to Intermediate in Wood Canyon with Liza and David (Repeats every Thu)</a>
-    <div class="rsvp">15 OC Hikers & Backpackers! going </div>
+    <div class="rsvp"></div>
 </div>*/
 function renderMeetUpOnDom(meetup){
-    var group = meetup.name;
     for(let m=0; m<meetup.length;m++){
         let groupName = $('<h4>',{
-        class: 'groupName',
-        text: filteredMeetUpResults[m].group.name})
+            class: 'groupName',
+            text: meetup[m].group.name})
         let members = $('<div>',{
             class: 'rsvp',
-            text: filteredMeetUpResults[m].group.who})
-        let time = $('<h4>',{
+            text: `${meetup[m].yes_rsvp_count} ${meetup[m].group.who} going`})
+        let time = $('<div>',{
+            class: 'time',
+            text: meetup[m].time})
+        let eventName = $('<a>',{
             class: 'rsvp',
-            text: filteredMeetUpResults[m].group.who})
-        let eventName = $('<h4>',{
-            class: 'groupName',
-            text: filteredMeetUpResults[m].eventName
-        var link = $('<h4>',{
-            class: 'groupName',
-            text: filteredMeetUpResults[m].eventName.link
-        var meetupDiv = $('<div>').addClass(`meetUp+${m}`);
+            text: meetup[m].eventName,
+            href: meetup[m].link})
+        let meetUp = $('.meetup');
+        let meetupDiv = $('<div>').addClass(`meetUp+${m}`);
+        meetupDiv = $(meetupDiv).append(time,groupName,eventName,members)
+        $(meetUp).append(meetupDiv)
     }
 }
