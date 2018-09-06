668bc5af18311c617b0e2ca4efbb1ee3b6f78769
Esther <esther2180@gmail.com>
2018-09-06 12:15:39 -0700

    align result list and picture div

--- Perry_functions.js
+++ Perry_functions.js
@@ -1,13 +1,20 @@
 function renderInformationOnDom(runningTrailsArray) {
 
     for ( let i = 1; i < runningTrailsArray.length; i++) {
-        console.log(runningTrailsArray[i]);
+        // console.log(runningTrailsArray[i]);
         let listResultsDiv = $('<div>').addClass('list_result');
-        let imageOfPlace = $('<img>').attr('src', runningTrailsArray[i].image);
-        let nameOfPlace = $('<h3>').text(runningTrailsArray[i].name);
-        let addressOfPlace = $('<p>').text(`${runningTrails[i].location.display_address[0]} ${runningTrails[i].location.display_address[1]}`);
+        let locationPictureDiv = $('<div>');
+        let imageOfPlace = $('<img>').attr('src', runningTrailsArray[i].image).addClass('locationPicture');
+        locationPictureDiv.append(imageOfPlace);
+        let locationDescriptionDiv = $('<div>').addClass('locationDescription');
+        let nameOfPlace = $('<p>').text(runningTrailsArray[i].name);
+        let addressOfPlace1 = `${runningTrails[i].location.display_address[0]}`;
+        let brl = $('<br>');
+        let addressOfPlace2 = `${runningTrails[i].location.display_address[1]}`;
+        let addressOfPlace = $('<adress>').append(addressOfPlace1, brl, addressOfPlace2);
         let moreInfoButton = $('<button>').text('More Info');
-        listResultsDiv.append(nameOfPlace, imageOfPlace, addressOfPlace, moreInfoButton);
+        locationDescriptionDiv.append(nameOfPlace, addressOfPlace, moreInfoButton);
+        listResultsDiv.append(locationPictureDiv, locationDescriptionDiv);
         $('.location_list').append(listResultsDiv);
     }
 }
