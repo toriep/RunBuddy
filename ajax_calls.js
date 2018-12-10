0e69e3a84a75661399d1ebe301ab8758b5f682a7
Torie Pham <tientp@uci.edu>
2018-12-09 22:16:36 -0800

    disabled weather forecast

--- ajax_calls.js
+++ ajax_calls.js
@@ -31,7 +31,7 @@ function responseFromGeolocation(response) {
     runningTrails.push(center);
     getDataFromTrailsList(lat, lng);
     getDataFromWeather(lat, lng);
-    getWeatherForecast(lat, lng);
+    // getWeatherForecast(lat, lng);
 }
 
 function getCurrentLocationForDirection() {
@@ -103,16 +103,16 @@ function getDataFromWeather(lat, lon) {
     $.ajax(weather);
 }
 
-function getWeatherForecast(lat, lon) {
-    const forecast = {
-        url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${WEATHER_API_KEY}`,
-        method: 'post',
-        dataType: 'json',
-        success: displayForecastSuccess,
-        error: ()=>displayError('getWeatherForecast'),
-    }
-    $.ajax(forecast);
-}
+// function getWeatherForecast(lat, lon) {
+//     const forecast = {
+//         url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${WEATHER_API_KEY}`,
+//         method: 'post',
+//         dataType: 'json',
+//         success: displayForecastSuccess,
+//         error: ()=>displayError('getWeatherForecast'),
+//     }
+//     $.ajax(forecast);
+// }
 
 function getDataFromMeetUp(lat, long, trailName) {
     const meetup = {
